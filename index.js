70c2504757fa1a76a4fd456eeabe3d34fd31e9b3
kevinismail <136602357+kevinismail@users.noreply.github.com>
2024-02-17 18:39:40 +0100

    Update index.js
    API Endpoint : Le code définit une route GET pour /api/:date? qui accepte une date optionnelle en tant que paramètre.
    
    -  Si aucune date n'est spécifiée, la date actuelle est utilisée.
    -  Si une date est spécifiée, elle est convertie en un objet Date de JavaScript.
    -  Si la date est un timestamp Unix, elle est convertie en un objet Date.
    -  Si la date n'est pas valide, une réponse JSON avec une erreur est renvoyée. 
    - Sinon, la date est convertie en timestamp Unix et en chaîne de date UTC, et ces valeurs sont renvoyées dans une réponse JSON.

--- index.js
+++ index.js
@@ -20,8 +20,39 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+
+  let unixDate;
+  let dateObj;
+  let utcDate;
+
+  //Test wether the inpute date is a number or not 
+let isUnix = /^\d+$/.test(date);
+// if no date specified then use the current date
+if (!date) {
+  dateObj = new Date();
+} 
+//if the date is a unix 
+else if (date && isUnix) {
+
+  unixDate = parseInt(date);
+  dateObj = new Date (unixDate);
+}
+//if the date isnt a unix timestamp
+else if (!isUnix) {
+  dateObj = new Date(date);
+}
+
+if (dateObj.toString() === "Invalid Date"){
+  res.json({error: "Invalid Date"});
+  return;
+}
+  unixDate = dateObj.getTime();
+  utcDate = dateObj.toUTCString();
+
+
+  res.json({unix: unixDate, utc: utcDate});
 });
 
 
